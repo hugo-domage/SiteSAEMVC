33bbd47d3678ba2e44396ed49d885bf5c0ceb4f8
hugo-domage <hugo.domage-cicchelero@etu.univ-amu.fr>
2023-03-30 19:52:54 +0200

    Suppression des anciens fichiers parasite

--- models/QuestionreponseManager.php
+++
@@ -1,7 +0,0 @@
-<?php
-class QuestionreponseManager extends Model{
-    public function getQuestions(){
-        $this->getBdd();
-        return $this->getAll('t_question_reponse','Question');
-    }
-}
