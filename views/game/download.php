5647207cf5e149d337bdd6a7938aecdb3ac155ba
Hugo DOMAGÉ <91956673+hugo-domage@users.noreply.github.com>
2023-04-03 23:58:36 +0200

    Changement de lien download

--- views/game/download.php
+++ views/game/download.php
@@ -1,3 +1,6 @@
 <body>
-<a href="https://aixmarseilleuniversite-my.sharepoint.com/:u:/g/personal/melvin_tomezak_etu_univ-amu_fr/ETtnz3Ab99tFru6EPfy_bMwBtIfS-CLE8yJRKaWk8FCalg?e=nerwkj">Link to the Application folder</a>;
+<p>.JAR</p>
+<a href="https://aixmarseilleuniversite-my.sharepoint.com/personal/alex_sarti_etu_univ-amu_fr/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Falex%5Fsarti%5Fetu%5Funiv%2Damu%5Ffr%2FDocuments%2FPi%C3%A8ces%20jointes%2FSAELAVRAI%2D1%2E0%2DSNAPSHOT%201%2Ejar&parent=%2Fpersonal%2Falex%5Fsarti%5Fetu%5Funiv%2Damu%5Ffr%2FDocuments%2FPi%C3%A8ces%20jointes&ct=1680558750167&or=OWA%2DNT&cid=44b4b18a%2D185b%2Ddea6%2D87f8%2D136244db382a&ga=1">Link to the Application folder</a>;
+<p> solution de secours</p>
+<a href="https://aixmarseilleuniversite-my.sharepoint.com/personal/melvin_tomezak_etu_univ-amu_fr/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmelvin%5Ftomezak%5Fetu%5Funiv%2Damu%5Ffr%2FDocuments%2FPi%C3%A8ces%20jointes%2FCode%20Applicationtest%2Ezip&parent=%2Fpersonal%2Fmelvin%5Ftomezak%5Fetu%5Funiv%2Damu%5Ffr%2FDocuments%2FPi%C3%A8ces%20jointes&ct=1680558656192&or=OWA%2DNT&cid=d8073f00%2Da31e%2D432f%2D544b%2D631351c24053&ga=1">Link to the Application folder </a>
 </body>
